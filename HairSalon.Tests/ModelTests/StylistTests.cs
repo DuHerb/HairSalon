a33976d901d0d9a30764986cbac2c388bf8a0357
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-05-10 11:58:48 -0700

    build all Stylist class get property tests

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -18,8 +18,38 @@ namespace HairSalon.TestTools
         [TestMethod]
         public void StylistConstructor_CreatesInstanceOfStylist_Stylist()
         {
-            Stylist newStylist = new Stylist("Mike", "Beard");
+            Stylist newStylist = new Stylist();
             Assert.AreEqual(typeof(Stylist), newStylist.GetType());
+        }
+
+        [TestMethod]
+        public void GetPropertyFirstName_ReturnsFirstName_String()
+        {
+            string firstName = "Mike";
+            Stylist newStylist = new Stylist("Mike");
+
+            string result = newStylist.FirstName;
+            Assert.AreEqual(firstName, result);
+        }
+
+        [TestMethod]
+        public void GetPropertyLastName_ReturnsLastName_String()
+        {
+            string lastName = "beard";
+            Stylist newStylist = new Stylist("mike", "beard");
+
+            string result = newStylist.LastName;
+            Assert.AreEqual(lastName, result);
+        }
+
+        [TestMethod]
+        public void GetPropertyId_ReturnsId_Int()
+        {
+            int id = 1;
+            Stylist newStylist = new Stylist("mike", "beard", 1);
+
+            int result = newStylist.Id;
+            Assert.AreEqual(id, result);
         }
     }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,14 +1,18 @@
-namespace HairSalon
+namespace HairSalon.Models
 {
     public class Stylist
     {
         public string FirstName {get;set;}
         public string LastName {get;set;}
+        int _id;
 
-        public Stylist(string firstName, string lastName)
+        public int Id {get=> _id;}
+
+        public Stylist(string firstName = "default", string lastName = "default", int id = 0)
         {
             FirstName = firstName;
             LastName = lastName;
+            _id = id;
         }
     }
 }
